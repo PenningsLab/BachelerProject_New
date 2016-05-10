cae475e3b9e49e2e889201b2cba59f2c13a000e2
pleunipennings <pspennings@gmail.com>
2016-05-09 20:44:42 -0700

    updated Stanford Bacheler comparison

--- Comparison-stanford_vs_bacheler-april2016/CalculatePlotR2StanfordBacheler.R
+++ Comparison-stanford_vs_bacheler-april2016/CalculatePlotR2StanfordBacheler.R
@@ -15,54 +15,73 @@ OverviewDFLehman$StanfordFreqs<-t(read.csv("subtypeB-frequencies_stanford_pr_rt_
 
 which(consensusA==consensusB& consensusC==consensusB)
 
-# plots all Bacheler
+# plots all comparisons
+
 #Take only syn and nonsyn sites after 13 AA of protease
 synsites<-which(OverviewDFBach$TypeOfSite=="syn"&OverviewDFBach$num>39&consensusA==consensusB&consensusC==consensusB)
 nonsynsites<-which(OverviewDFBach$TypeOfSite=="nonsyn"&OverviewDFBach$num>39&consensusA==consensusB&consensusC==consensusB)
 
-plot(OverviewDFBach$colMeansTs0,OverviewDFBach$StanfordFreqs,
-     main="Within vs between-host frequencies",col=0,
-     xlim=c(0,.4),ylim=c(0,.4),
-     xlab="Within-host frequencies Bacheler",
-     ylab="Between-host frequencies Stanford"
-     )
-points(OverviewDFBach$colMeansTs0[synsites],OverviewDFBach$StanfordFreqs[synsites],
-       col="lightgreen",pch=16)
-points(OverviewDFBach$colMeansTs0[nonsynsites],OverviewDFBach$StanfordFreqs[nonsynsites],
-       col="orange",pch=16)
-
-R2BachStanford<-cor.test(OverviewDFBach$colMeansTs0[c(synsites,nonsynsites)],OverviewDFBach$StanfordFreqs[c(synsites,nonsynsites)])$estimate
-text(0.05,0.3,paste("R^2 = ",round(R2BachStanford,2)))
-
 # plots all Zanini
-plot(OverviewDFZanini$colMeansTsZanini,OverviewDFZanini$StanfordFreqs,
+plot(OverviewDFZanini$colMeansTsZanini+0.0001,OverviewDFZanini$StanfordFreqs+0.0001,
      main="Within vs between-host frequencies Zanini",col=0,
-     xlim=c(0,.4),ylim=c(0,.4),
+     xlim=c(0.0001,1),ylim=c(0.0001,1),
      xlab="Within-host frequencies Zanini",
-     ylab="Between-host frequencies Stanford"
+     ylab="Between-host frequencies Stanford",
+    log="xy"
 )
-points(OverviewDFZanini$colMeansTsZanini[synsites],OverviewDFZanini$StanfordFreqs[synsites],
+points(OverviewDFZanini$colMeansTsZanini[synsites]+0.0001,OverviewDFZanini$StanfordFreqs[synsites]+0.0001,
        col="lightgreen",pch=16)
-points(OverviewDFZanini$colMeansTsZanini[nonsynsites],OverviewDFZanini$StanfordFreqs[nonsynsites],
+points(OverviewDFZanini$colMeansTsZanini[nonsynsites]+0.0001,OverviewDFZanini$StanfordFreqs[nonsynsites]+0.0001,
        col="orange",pch=16)
 
-R2ZaniniStanford<-cor.test(OverviewDFZanini$colMeansTsZanini[c(synsites,nonsynsites)],OverviewDFZanini$StanfordFreqs[c(synsites,nonsynsites)])$estimate
+CorZaniniStanford<-cor.test(OverviewDFZanini$colMeansTsZanini[c(synsites,nonsynsites)],OverviewDFZanini$StanfordFreqs[c(synsites,nonsynsites)])$estimate
 text(0.05,0.3,paste("R^2 = ",round(R2ZaniniStanford,2)))
 
-
 # plots all Lehman
-plot(OverviewDFLehman$colMeansTsLehman,OverviewDFLehman$StanfordFreqs,
+plot(OverviewDFLehman$colMeansTsLehman+0.0001,OverviewDFLehman$StanfordFreqs+0.0001,
      main="Within vs between-host frequencies Lehman",col=0,
-     xlim=c(0,.4),ylim=c(0,.4),
+     xlim=c(0.0001,1),ylim=c(0.0001,1),
      xlab="Within-host frequencies Lehman",
-     ylab="Between-host frequencies Stanford"
+     ylab="Between-host frequencies Stanford",
+     log="xy"
 )
-points(OverviewDFLehman$colMeansTsLehman[synsites],OverviewDFLehman$StanfordFreqs[synsites],
+points(OverviewDFLehman$colMeansTsLehman[synsites]+0.0001,OverviewDFLehman$StanfordFreqs[synsites]+0.0001,
        col="lightgreen",pch=16)
-points(OverviewDFLehman$colMeansTsLehman[nonsynsites],OverviewDFLehman$StanfordFreqs[nonsynsites],
+points(OverviewDFLehman$colMeansTsLehman[nonsynsites]+0.0001,OverviewDFLehman$StanfordFreqs[nonsynsites]+0.0001,
        col="orange",pch=16)
 
 R2LehmanStanford<-cor.test(OverviewDFLehman$colMeansTsLehman[c(synsites,nonsynsites)],OverviewDFLehman$StanfordFreqs[c(synsites,nonsynsites)])$estimate
 text(0.05,0.3,paste("R^2 = ",round(R2LehmanStanford,2)))
 
+#Plot Bacheler
+plot(OverviewDFBach$colMeansTs0+0.0001,OverviewDFBach$StanfordFreqs+0.0001,
+     main="Within vs between-host frequencies Bacheler",col=0,
+     xlim=c(0.0001,1),ylim=c(0.0001,1),
+     xlab="Within-host frequencies Bacheler",
+     ylab="Between-host frequencies Stanford",
+     log="xy"
+)
+points(OverviewDFBach$colMeansTs0[synsites]+0.0001,OverviewDFBach$StanfordFreqs[synsites]+0.0001,
+       col="lightgreen",pch=16)
+points(OverviewDFBach$colMeansTs0[nonsynsites]+0.0001,OverviewDFBach$StanfordFreqs[nonsynsites]+0.0001,
+       col="orange",pch=16)
+
+R2BachStanford<-cor.test(OverviewDFBach$colMeansTs0[c(synsites,nonsynsites)],OverviewDFBach$StanfordFreqs[c(synsites,nonsynsites)])$estimate
+text(0.05,0.3,paste("R^2 = ",round(R2BachStanford,2)))
+
+#Plot Bacheler RT only
+plot(OverviewDFBach$colMeansTs0+0.0001,OverviewDFBach$StanfordFreqs+0.0001,
+     main="Within vs between-host frequencies Bacheler",col=0,
+     xlim=c(0.0001,1),ylim=c(0.0001,1),
+     xlab="Within-host frequencies Bacheler",
+     ylab="Between-host frequencies Stanford",
+     log="xy"
+)
+points(OverviewDFBach$colMeansTs0[synsites[which(synsites>297)]]+0.0001,OverviewDFBach$StanfordFreqs[synsites[which(synsites>297)]]+0.0001,
+       col="lightgreen",pch=16)
+points(OverviewDFBach$colMeansTs0[nonsynsites[which(nonsynsites>297)]]+0.0001,OverviewDFBach$StanfordFreqs[nonsynsites[which(nonsynsites>297)]]+0.0001,
+       col="orange",pch=16)
+
+R2BachStanford<-cor.test(OverviewDFBach$colMeansTs0[c(synsites,nonsynsites)],OverviewDFBach$StanfordFreqs[c(synsites,nonsynsites)])$estimate
+text(0.05,0.3,paste("R^2 = ",round(R2BachStanford,2)))
 
