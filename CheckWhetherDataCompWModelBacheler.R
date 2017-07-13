7f2571e4e6fff02c00be4283738965331d99ac59
pleunipennings <pspennings@gmail.com>
2017-07-13 10:16:27 -0700

    Worked on testing whether freq spec is as expected. When I set Ne to 10,000 I get around 8% pvalues <0.05 which seems quite OK.

--- CheckWhetherDataCompWModelBacheler.R
+++ CheckWhetherDataCompWModelBacheler.R
@@ -1,6 +1,8 @@
 #July 2017 , July 11th. 
 
 #My goal is to see if the observed Single Site Freq Spec (Bacheler data) fits with predicted for fitness cost paper. 
+library(scales)
+library(plotrix)
 
 setwd("~/Documents/Git/bachelerProject/SimulationsEstimatingSelCoeffSims/")
 system("./Code_and_shellscript/make_HIV1site")    #compile the code
@@ -26,8 +28,10 @@ for (i in 1:length(listfastafiles)){ #for each fastafile
 
 pdf ("comparisonDataSims.pdf")
 
+listPvalues<-c()
 #Choose a site 
-for (site in 15:110){
+for (site in 50:400){
+    print(site)
 realFreqs<-freqPatTsFilter[,site]
 
 #Mut rates and sel coefficients
@@ -38,7 +42,7 @@ cost=OverviewDF$EstSelCoeff[site]
 #SimulationsEstimatingSelCoeffSims
 #To create simulated data. 
 
-Ne = 100000 #currently Ne cannot be changed in the sims, has to be 10000 # 07/11/17 I changed it to 100000
+Ne = 10000 #currently Ne cannot be changed in the sims, has to be 10000 # 07/11/17 I changed it to 100000
 numoutputs = length(NumseqsBach) #(max num of patients, should be around 200)
 theta = mu*Ne
 
@@ -71,37 +75,60 @@ system("rm ./SimData/Data*")
 #Get sample Freqs
 #use the real n from the Bacheler data. 
 numoutputs<-length(which(!is.na(realFreqs)))
-Freqs<-rbinom(numoutputs,NumseqsBach[!is.na(realFreqs)],Freqs)/NumseqsBach[!is.na(realFreqs)]
+simFreqs<-rbinom(numoutputs,NumseqsBach[!is.na(realFreqs)],Freqs)/NumseqsBach[!is.na(realFreqs)]
+realFreqs<-realFreqs[!is.na(realFreqs)]
 
 #Plot
 br<-seq(-10^-7,1.01,by=0.01)
-br2<-seq(0,1,by=0.05)
-xlims=c(0,0.01+max(c(Freqs,realFreqs),na.rm=TRUE))
-ylims = c(0,max(length(which(realFreqs==0)),length(which(Freqs==0))))
+br2<-seq(0,1,by=0.02)
+xlims=c(0,0.01+max(c(simFreqs,realFreqs),na.rm=TRUE))
+ylims = c(0,max(length(which(realFreqs==0)),length(which(simFreqs==0))))
 #ylims = c(0,5)
-
    
 par(mfrow=c(2,1))
+maxyheight=50
 
-hist(realFreqs,breaks=br,xlim=xlims, ylim = ylims,
+hist(rep(0,maxyheight),breaks=br,xlim=xlims, #ylim = ylims, 
      ylab="", xlab = "", main=paste("Single-SFS, site",site),
-     xaxt="n", col="blue"
-)
+     xaxt="n", yaxt="n",col=OverviewDFilter$color[site])
+hist(c(rep(0,min(maxyheight-10,length(which(realFreqs<0.01)))),realFreqs[which(realFreqs>=0.01)]), breaks=br, col=OverviewDFilter$color[site], add=TRUE)
+axis(2,labels = c(10,20,30,40,max(maxyheight,length(which(realFreqs<0.01)))), 
+          at = c(10,20,30,40,maxyheight), las=1)
+if (length(which(realFreqs<0.01))>=maxyheight){
+    axis.break(axis=2,breakpos=maxyheight-20,bgcol="white",breakcol="black",style="slash",brw=0.02)
+    rect(-0.001, maxyheight-12, 0.0101, maxyheight-8,col="white",border="white")
+    }else{axis(2,labels = maxyheight-10,at=maxyheight-10,las=1)}
+     
 axis(1, at=br2,labels=br2, col.axis="black", las=2)
-hist(rep(0,length(which(realFreqs==0))),breaks=br, add = T, col=1)
+#hist(rep(0,length(which(realFreqs==0))),breaks=br, add = T, col=1)
 mtext("Observed frequency", side=1, line=4, cex.lab=1,las=1, col="black")
 mtext("Number of patients", side=2, line=2.5, cex.lab=1,las=3, col="black")
 abline(v=mean(realFreqs,na.rm=TRUE),col=2)
 
-hist(Freqs,breaks=br,xlim=xlims,ylim = ylims,
-     ylab="", xlab = "", main=paste("Simulated Data, cost",round(cost,5)),
-     xaxt="n", col="blue"
-)
+hist(rep(0,maxyheight),breaks=br,xlim=xlims, #ylim = ylims, 
+     ylab="", xlab = "", main=paste("Simulated data, cost",round(cost,4)),
+     xaxt="n", yaxt="n",col="pink")
+hist(c(rep(0,min(maxyheight-10,length(which(simFreqs<0.01)))),simFreqs[which(simFreqs>=0.01)]), breaks=br, col="pink", add=TRUE)
+axis(2,labels = c(10,20,30,40,max(maxyheight,length(which(simFreqs<0.01)))), 
+     at = c(10,20,30,40,maxyheight), las=1)
+if (length(which(simFreqs<0.01))>=maxyheight){
+    axis.break(axis=2,breakpos=maxyheight-20,bgcol="white",breakcol="black",style="slash",brw=0.02)
+    rect(-0.001, maxyheight-12, 0.0101, maxyheight-8,col="white",border="white")
+}else{axis(2,labels = maxyheight-10,at=maxyheight-10,las=1)}
+
+text(x=mean(xlims),y=maxyheight/2,paste("pvalue=",round(wilcox.test(simFreqs,realFreqs)[[3]],3)))
 axis(1, at=br2,labels=br2, col.axis="black", las=2)
-hist(rep(0,length(which(Freqs==0))),breaks=br, add = T, col=1)
+#hist(rep(0,length(which(simFreqs==0))),breaks=br, add = T, col=2)
 mtext("Observed frequency", side=1, line=4, cex.lab=1,las=1, col="black")
 mtext("Number of patients", side=2, line=2.5, cex.lab=1,las=3, col="black")
-abline(v=mean(Freqs),col=2)
+abline(v=mean(simFreqs),col=2)
+
+listPvalues<-c(listPvalues,wilcox.test(simFreqs,realFreqs)[[3]])
+
 }
 
 dev.off()
+
+qqnorm(listPvalues)
+qqline(listPvalues)
+length(which(listPvalues<.05))/length(listPvalues)
