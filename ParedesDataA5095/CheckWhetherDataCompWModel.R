b5861d3d04aad328278f9763c3518e1d1e688630
pleunipennings <pspennings@gmail.com>
2017-11-16 09:29:31 -0800

    did some work on the Paredes data for Zandrea. Unrelated to fitness cost paper.

--- ParedesDataA5095/CheckWhetherDataCompWModel.R
+++ ParedesDataA5095/CheckWhetherDataCompWModel.R
@@ -3,7 +3,7 @@
 #My goal is to see if the observed Single Site Freq Spec fits with predicted for fitness cost paper. 
 
 setwd("~/Documents/Git/bachelerProject/ParedesDataA5095")
-system("./Code_and_shellscript/make_HIV1site")    #compile the code
+#system("./Code_and_shellscript/make_HIV1site")    #compile the code
 system("rm ./SimData/Data*")
 
 Data<-read.csv("MeanFreqDataRanSubCohort.csv")
@@ -12,16 +12,19 @@ Freqs103AAC<-Data$K103AACFreq[!is.na(Data$K103AACFreq)]
 Freqs103AAT<-Data$K103AATFreq[!is.na(Data$K103AATFreq)]
 #Freqs103AAT<-sort(Freqs103AAT)[1:(length(Freqs103AAT)-2)]
 
-
 #Mut rates and sel coefficients
 read.csv("../Data/HIVMutRates/HIVMutRates.csv")->mutrates
 #AC mut rate
 mutrates$Probability[mutrates$Nucleotide.substitution=="AC"]
 
-#mu_AC=mutrates$Probability[mutrates$Nucleotide.substitution=="AC"]
-#mu_AT=mutrates$Probability[mutrates$Nucleotide.substitution=="AU"]/5
-mu_AC=9*10^-7
-mu_AT=(7*10^-7)
+mu_AC=mutrates$Probability[mutrates$Nucleotide.substitution=="AC"]
+mu_AT=mutrates$Probability[mutrates$Nucleotide.substitution=="AU"] #divide by 5?
+#mu_AC=9*10^-7
+mu_AC_Zan=mutrates$ZaniniProb[mutrates$Nucleotide.substitution=="AC"]
+mu_AT_Zan=mutrates$ZaniniProb[mutrates$Nucleotide.substitution=="AU"]/5
+
+
+#mu_AT=(7*10^-7)
 #mu_AT=(7*10^-7)/5
 s_AAC<-mu_AC/mean(Freqs103AAC)
 s_AAT<-mu_AT/mean(Freqs103AAT)
@@ -31,7 +34,7 @@ s_AAT<-mu_AT/mean(Freqs103AAT)
 #SimulationsEstimatingSelCoeffSims
 #To create simulated data. 
 
-Ne = 300000 #currently Ne cannot be changed in the sims, has to be 10000 # 07/11/17 I changed it to 100000
+Ne = 300000 #currently Ne cannot be changed in the sims, currently 300000 Nov 2017
 nmax = 200 #(max num of patients, should be around 200)
 theta_AC = mu_AC*Ne
 theta_AT = mu_AT*Ne
@@ -40,7 +43,7 @@ NUMRUNS=1; numoutputs=nmax
 #for AAC
 if (TRUE){ #create data through simulations
     numsites = 1
-    cost = s_AAC
+    cost = s_AAC; theta = theta_AC
     seed =100
     #make script 
     x<-"#!/bin/bash"
@@ -73,7 +76,7 @@ system("rm ./SimData/Data*")
 #for AAT
 if (TRUE){ #create data through simulations
     numsites = 1
-    cost = s_AAT
+    cost = s_AAT; theta = theta_AT
     seed =100
     #make script 
     x<-"#!/bin/bash"
@@ -104,15 +107,20 @@ read.csv(filename,sep="\t")$freq->PopFreqs103AAT_sim
 
 
 #Get sample Freqs
-#n=500000
-#Freqs103AAC_sim<-rbinom(200,n,PopFreqs103AAC_sim)/n
+numparticles=50000
+Freqs103AAC_sim<-rbinom(n=200,size=numparticles,prob=PopFreqs103AAC_sim)/numparticles
+Freqs103AAT_sim<-rbinom(n=200,size=numparticles,PopFreqs103AAT_sim)/numparticles
 #Plot
 br<-seq(-0.00001+10^-7,0.1,by=0.00001)
 
+if (TRUE){
+xlimset=c(-0.00001+10^-7,0.0008)    
+    
+pdf("ParedesData_Sims.pdf",width = 10, height = 10)
 par(mfrow=c(2,2))
 #png("Freqs103AAC.png")
-hist(Freqs103AAC,xlim=c(-0.00001+10^-7,0.0002),breaks=br, 
-     ylab="", xlab = "", main="Single-Site Frequency Spectrum RT103AAC",
+hist(Freqs103AAC,xlim=xlimset,breaks=br, 
+     ylab="", xlab = "", main="SFS RT103AAC",
      xaxt="n", col="blue"
 )
 axis(1, at=c(round(br[2],5)-0.000005,round(br[3:16],5)),labels=round(br[2:16],5), col.axis="black", las=2)
@@ -123,18 +131,18 @@ abline(v=mean(Freqs103AAC),col=2)
 
 #dev.off()
 
-hist(PopFreqs103AAC_sim,xlim=c(-0.00001+10^-7,0.0002),breaks=br, 
-     ylab="", xlab = "", main="Simul.Single-Site Frequency Spectrum RT103AAC",
+hist(Freqs103AAC_sim,xlim=xlimset,breaks=br, 
+     ylab="", xlab = "", main="Simul.SFS RT103AAC",
      xaxt="n", col="blue"
 )
 axis(1, at=c(round(br[2],5)-0.000005,round(br[3:16],5)),labels=round(br[2:16],5), col.axis="black", las=2)
 hist(rep(0,length(which(PopFreqs103AAC_sim==0))),breaks=br, add = T, col=1)
 mtext("Observed frequency", side=1, line=4, cex.lab=1,las=1, col="black")
 mtext("Number of patients", side=2, line=2.5, cex.lab=1,las=3, col="black")
-abline(v=mean(PopFreqs103AAC_sim),col=2)
+abline(v=mean(Freqs103AAC_sim),col=2)
 
-hist(Freqs103AAT,xlim=c(-0.00001+10^-7,0.0002),breaks=br, 
-     ylab="", xlab = "", main="Single-Site Frequency Spectrum RT103AAT",
+hist(Freqs103AAT,xlim=xlimset,breaks=br, 
+     ylab="", xlab = "", main="SFS RT103AAT",
      xaxt="n", col="blue"
 )
 axis(1, at=c(round(br[2],5)-0.000005,round(br[3:16],5)),labels=round(br[2:16],5), col.axis="black", las=2)
@@ -143,16 +151,16 @@ mtext("Observed frequency", side=1, line=4, cex.lab=1,las=1, col="black")
 mtext("Number of patients", side=2, line=2.5, cex.lab=1,las=3, col="black")
 abline(v=mean(Freqs103AAT),col=2)
 
-hist(PopFreqs103AAT_sim,xlim=c(-0.00001+10^-7,0.0002),breaks=br, 
-     ylab="", xlab = "", main="Simul.Single-Site Frequency Spectrum RT103AAT",
+hist(Freqs103AAT_sim,xlim=xlimset,breaks=br, 
+     ylab="", xlab = "", main="Simul.SFS RT103AAT",
      xaxt="n", col="blue"
 )
 axis(1, at=c(round(br[2],5)-0.000005,round(br[3:16],5)),labels=round(br[2:16],5), col.axis="black", las=2)
 hist(rep(0,length(which(PopFreqs103AAT_sim==0))),breaks=br, add = T, col=1)
 mtext("Observed frequency", side=1, line=4, cex.lab=1,las=1, col="black")
 mtext("Number of patients", side=2, line=2.5, cex.lab=1,las=3, col="black")
-abline(v=mean(PopFreqs103AAT_sim),col=2)
+abline(v=mean(Freqs103AAT_sim),col=2)
 
 
-
-
+dev.off()
+}
--- ParedesDataA5095/Code_and_shellscript/tempscript.sh
+++ ParedesDataA5095/Code_and_shellscript/tempscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mu=7e-07
-output_every_Xgen=343
-numgen_inN=0.230953333333333
-start_output=0.00228666666666667
-cost=0.0145965306302669
+mu=7.93e-07
+output_every_Xgen=303
+numgen_inN=0.20402
+start_output=0.00202
+cost=0.0165357839854309
 for seed in 100
 do
 echo "
@@ -13,5 +13,5 @@ $cost
 $output_every_Xgen
 $numgen_inN
 $start_output
-" | ./Code_and_shellscript/HIVevolution_HIV1site >./SimData/Data_T_0.3172776_cost_0.0145965306302669.txt
+" | ./Code_and_shellscript/HIVevolution_HIV1site >./SimData/Data_T_0.2379_cost_0.0165357839854309.txt
 done
